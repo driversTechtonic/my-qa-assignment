e1ea97bd236ab52a5911b78d5f897b1fd0c37474
driversTechtonic <danielle.rivers@techtoic.com>
2021-10-08 16:12:18 -0500

    Updated Cypress tests

--- cypress-realworld-app/cypress/tests/mine/createaccount.spec.js
+++ cypress-realworld-app/cypress/tests/mine/createaccount.spec.js
@@ -6,20 +6,20 @@ describe("Create Account", () => {
   });
 
   it.only("new account test", () => {
-    cy.get("#firstName").type("Danielle");
-    cy.get("#lastName").type("Rivers");
-    cy.get("#username").type("Drivers");
-    cy.get("#password").type("aQua4rce");
-    cy.get("#confirmPassword").type("aQua4rce");
+    cy.get("#firstName").type("Danielle").should("have.value", "Danielle");
+    cy.get("#lastName").type("Rivers").should("have.value", "Rivers");
+    cy.get("#username").type("Drivers").should("have.value", "Drivers");
+    cy.get("#password").type("aQua4rce").should("have.value", "aQua4rce");
+    cy.get("#confirmPassword").type("aQua4rce").should("have.value", "aQua4rce");
     cy.get("[data-test=signup-submit]").click();
   });
 
   it.only("confirm password fail test", () => {
-    cy.get("#firstName").type("Danielle");
-    cy.get("#lastName").type("Rivers");
-    cy.get("#username").type("Drivers");
-    cy.get("#password").type("aQua4rce");
-    cy.get("#confirmPassword").type("aQua3rce");
+    cy.get("#firstName").type("Danielle").should("have.value", "Danielle");
+    cy.get("#lastName").type("Rivers").should("have.value", "Rivers");
+    cy.get("#username").type("Drivers").should("have.value", "Drivers");
+    cy.get("#password").type("aQua4rce").should("have.value", "aQua4rce");
+    cy.get("#confirmPassword").type("aQua3rce").should("have.value", "aQua3rce");
   });
 
   it.only("empty fields test", () => {
--- cypress-realworld-app/cypress/tests/mine/login.spec.js
+++ cypress-realworld-app/cypress/tests/mine/login.spec.js
@@ -6,14 +6,14 @@ describe("Login", () => {
   });
 
   it.only("login test", () => {
-    cy.get("#username").type("Katharina_Bernier");
-    cy.get("#password").type("s3cret");
+    cy.get("#username").type("Katharina_Bernier").should("have.value", "Katharina_Bernier");
+    cy.get("#password").type("s3cret").should("have.value", "s3cret");
     cy.get("[data-test=signin-submit]").click();
   });
 
   it.only("invalid login test", () => {
     cy.get("#username").type("Katharina_Berry");
     cy.get("#password").type("s4cret");
-    cy.get("[data-test=signin-submit]").click();
+    cy.get(".makeStyles-form-4").submit();
   });
 });
